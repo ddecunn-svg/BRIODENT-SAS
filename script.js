5d4abf8df38a5a9601d2256f7382ec6c67debebe
ddecunn-svg <ddecunn@gmail.com>
2026-02-17 00:52:12 -0500

    Create script.js for frontend interactions

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Example function to log current date and time
+function logCurrentDateTime() {
+    const currentDateTime = new Date().toISOString();
+    console.log('Current Date and Time (UTC):', currentDateTime);
+}
+
+// Call the function
+logCurrentDateTime();
